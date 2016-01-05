c2059c9dce95dbe5277b898489f2afe4e764a10f
pruvot_a <amaury.pruvot@gmail.com>
2016-01-05 21:48:56 +0100

    clean old code

--- BlackStarV2/src/fr/sigl/intg/controller/LoginController.java
+++ BlackStarV2/src/fr/sigl/intg/controller/LoginController.java
@@ -12,30 +12,6 @@ import fr.sigl.intg.LoginForm;
  */
 public class LoginController {
 
-	/*public Boolean login(Connection db, String login, String password) {
-		Boolean result = false;
-		try {
-			Statement st = db.createStatement( 
-					ResultSet.TYPE_SCROLL_SENSITIVE, 
-					ResultSet.CONCUR_READ_ONLY);
-			String sql = String.format("SELECT * FROM userlogin WHERE user_login='%s' AND user_password='%s';",
-	                login,
-	                password);
-			ResultSet rs = st.executeQuery(sql);
-			while (rs.next()) {
-				String log = rs.getString("user_login");
-				if (rs.getString("user_login").equalsIgnoreCase(login))
-					result = true;
-			}
-			
-			rs.close();
-			st.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}*/
-	
 	public Boolean submitLogin(String login, String password) {
 		
 		LoginForm loginForm = new LoginForm();
